40350f925cef24c2da090f1892e692f3bad64bfd
David Craine <davidcraine@Davids-Mac-Studio.local>
2023-12-26 15:46:49 -0500

    add model

---
+++ db/migrate/20231226200338_create_distilleries.rb
@@ -0,0 +1,15 @@
+class CreateDistilleries < ActiveRecord::Migration[7.2]
+  def change
+    create_table :distilleries do |t|
+      t.string :owner_name
+      t.string :operator_name
+      t.string :permit_number
+      t.string :street
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :county
+      t.timestamps
+    end
+  end
+end
