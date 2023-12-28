b1403775bd322bcdb9d024568112bdddf2067d7e
David Craine <davidcraine@Davids-Mac-Studio.local>
2023-12-27 19:11:03 -0500

    add distillery seed script

---
+++ db/seeds/distilleries.rb
@@ -0,0 +1,10 @@
+require 'csv'
+
+Distillery.transaction do
+  csv_path = File.path(Rails.root.join('lib', 'seeds', 'distilleries.csv'))
+  CSV.foreach(csv_path, headers: true) do |row|
+      Distillery.create(
+          row.to_h.transform_keys{|key| key.downcase.parameterize(separator: '_')}
+      )
+  end
+end
