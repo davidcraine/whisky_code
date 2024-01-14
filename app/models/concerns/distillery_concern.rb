3c46db591312cbfe2249b196403a24a85fe2a129
David Craine <davidcraine@Davids-Mac-Studio.local>
2024-01-14 16:56:17 -0500

    incorporate google maps using credentials to store the key

--- app/models/concerns/distillery_concern.rb
+++ app/models/concerns/distillery_concern.rb
@@ -1,6 +1,11 @@
 module DistilleryConcern
   extend ActiveSupport::Concern
 
+  def google_map_embed_link
+    api_key = Rails.application.credentials.google_maps_api_key
+    "https://www.google.com/maps/embed/v1/place?key=#{api_key}&q=#{google_map_address}"
+  end
+
   # a pararameterize version of an addressed that can be passed to the google maps url
   def google_map_address
     attributes.slice('street', 'city', 'state', 'zip').values.join('+')
