dd0ef562f061911f26db99729e58fced8020b53e
David Craine <davidcraine@Davids-Mac-Studio.local>
2024-01-15 12:13:09 -0500

    use turbo frames to load google map

--- app/controllers/distilleries_controller.rb
+++ app/controllers/distilleries_controller.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# Distillieries Controller
 class DistilleriesController < ApplicationController
-  before_action :set_distillery, only: %i[ show ]
+  before_action :set_distillery, only: %i[show load_gmap_iframe]
 
   # GET /distilleries or /distilleries.json
   def index
@@ -7,17 +10,22 @@ class DistilleriesController < ApplicationController
   end
 
   # GET /distilleries/1 or /distilleries/1.json
-  def show
+  def show; end
+
+  def load_gmap_iframe
+    @show_iframe = true
+    render turbo_stream: turbo_stream.replace('iframes', partial: 'gmap_iframe')
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_distillery
-      @distillery = Distillery.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def distillery_params
-      params.fetch(:test, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_distillery
+    @distillery = Distillery.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def distillery_params
+    params.fetch(:test, {})
+  end
 end
