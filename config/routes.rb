d7c604d7b97fff5c49fae1a9f61489ad1ccdd23b
David Craine <davidcraine@Davids-Mac-Studio.local>
2024-01-19 13:02:28 -0500

    for now set the root route to the distilleries controller index until we can build a welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # Defines the root path route ("/")
+  root 'distilleries#index'
+
   resources :distilleries, only: %i[index show] do
     member do
       post :load_gmap_iframe
