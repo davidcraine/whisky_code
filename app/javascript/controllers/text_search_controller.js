5bfb9020e431c140c8a515ce301186cefdef666e
David Craine <davidcraine@Davids-Mac-Studio.local>
2024-01-22 12:40:43 -0500

    hide the pagination bar when doing search text filtering

--- app/javascript/controllers/text_search_controller.js
+++ app/javascript/controllers/text_search_controller.js
@@ -2,15 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="text-search"
 export default class extends Controller {
+  paginationElement = undefined;
+
   connect() {
+    this.paginationElement = $('.pagination');
   }
 
   search(event) {
-    if (event.target.value.length >= 0) {
+
+    if (event.target.value.length > 0) {
+      this.paginationElement.hide();
       const url = new URL(window.location.href);
       url.search = '';  // clear the existing search params
       url.searchParams.set('query', event.target.value);
       Turbo.visit(url, { action: 'replace', frame: 'distilleries-frame' });
+    }
+    else {
+      this.paginationElement.show();
     }
   }
 }
