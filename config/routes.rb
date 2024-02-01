7e55dcab4e51bd52291cb62330e7f2dbdc721271
David Craine <davidcraine@Davids-Mac-Studio.local>
2024-02-01 12:31:18 -0500

    setup mission control for background job interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount MissionControl::Jobs::Engine, at: "/jobs"
+
   get "welcome/index"
   resources :tests
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
