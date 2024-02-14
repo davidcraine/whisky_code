e5d328367cd494942832dba8652a0b5a342468a1
David Craine <davidcraine@Davids-Mac-Studio.local>
2024-02-14 00:53:00 -0500

    working version with state selection capability

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,7 +1,16 @@
 class MapController < ApplicationController
+  include DistilleriesHelper
+
   def display; end
 
   def heatmap_data
     render json: Distillery.by_state(params[:state]).map(&:coordinates!)
   end
+
+  private
+
+  def current_state
+    params[:state]
+  end
+  helper_method :current_state
 end
--- app/javascript/controllers/google_map_controller.js
+++ app/javascript/controllers/google_map_controller.js
@@ -3,36 +3,48 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="google-map"
 export default class extends Controller {
   connect() {
+    this.initMap($('#dropdown').val())
+  }
+
+  initMap(state) {
     console.log("map controller connecting");
-    this.map = new google.maps.Map(this.element, {
+    this.map = new google.maps.Map($('#heatmap')[0], {
       center: { lat: 37.8, lng: -96 }, // Center map on the US
       zoom: 4 // Set initial zoom level
     });
 
-    // Load state data and create heat map layer
     const heatmapData = [];
-    // Add state data to heatmapData array
-    // Example: heatmapData.push({ location: new google.maps.LatLng(LATITUDE, LONGITUDE), weight: INTENSITY });
-    this.fetchHeatmapData()
+    this.fetchHeatmapData(state)
     .then(data => {
       if (data) {
         data.forEach(coordinates => {
-          console.log(coordinates)
           heatmapData.push({location: new google.maps.LatLng(coordinates[0], coordinates[1])})
       })
     }});
-  
-    //heatmapData.push({location: new google.maps.LatLng(36.7783, -119.4179), weight: 0.5})
+
     this.heatmap = new google.maps.visualization.HeatmapLayer({
       data: heatmapData,
       dissipating: false, // Allow the heat to dissipate
-      radius: 0.3, // Set the radius of influence for each data point
+      radius: 0.3, // Set the radius of influence for each data point,
       map: this.map
     });
   }
 
-  fetchHeatmapData() {
-    return fetch("/map/heatmap_data?state=CA")
+  // I can't seem to get the map to re-visualize when updating the heatmapt data
+  // so in this case we just redirect to the same page but with a new state param
+  // so the connect() method can re-initalize the map with the heatmap overaly
+  handleChange(event) {
+    const selectedValue = event.target.value
+    console.log(selectedValue)
+    console.log(window.location.href)
+    const url = new URL(window.location.href);
+    url.search = '';  // clear the existing search params
+    url.searchParams.set('state', selectedValue); 
+    window.location.href = url
+  }
+
+  fetchHeatmapData(state) {
+    return fetch("/map/heatmap_data?state="+state)
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
