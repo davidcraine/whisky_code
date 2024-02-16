e7b3c69d272e7178b5d9481ab69c31dbb5b19427
David Craine <davidcraine@Davids-Mac-Studio.local>
2024-02-16 16:16:33 -0500

    update seed data

--- db/seeds/distilleries.rb
+++ db/seeds/distilleries.rb
@@ -1,10 +1,10 @@
 require 'csv'
 
 Distillery.transaction do
-  csv_path = File.path(Rails.root.join('lib', 'seeds', 'distilleries.csv'))
+  csv_path = File.path(Rails.root.join('lib', 'seeds', 'geocoded_distilleries.csv'))
   CSV.foreach(csv_path, headers: true) do |row|
     Distillery.create(
-      row.to_h.transform_keys { |key| key.downcase.parameterize(separator: '_') }.transform_values { |value| value.gsub(/\r\n/, ' ') if value.is_a? String }
+      row.to_h.transform_keys { |key| key.downcase.parameterize(separator: '_') }
     )
   end
 end
